aa5f47f0bbc8b040f6c0ceb5151550c5cbd7822f
Cris-div <christian.unocc@tecsup.edu.pe>
2025-11-02 00:44:19 -0500

    Agregar characterService con función getCharacters para consumir API

--- src/services/characterService.js
+++ src/services/characterService.js
@@ -1,1 +1,10 @@
-//1
+import api from './api';
+
+export const getCharacters = async (params = {}) => {
+  try {
+    const response = await api.get('/character', { params });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
