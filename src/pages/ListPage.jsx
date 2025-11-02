0a7e394423fa47fa76f2ec2ff06f76643cb17d7f
Cris-div <christian.unocc@tecsup.edu.pe>
2025-11-02 01:18:39 -0500

    Agregar ListPage con listado de personajes, filtros y paginación

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,1 +1,34 @@
-//1
+import { useState } from 'react';
+import useCharacters from '../hooks/useCharacters';
+import CharacterCard from '../components/list/CharacterCard';
+import Pagination from '../components/common/Pagination';
+import FilterBar from '../components/list/FilterBar';
+import LoadingSpinner from '../components/common/LoadingSpinner';
+import ErrorAlert from '../components/common/ErrorAlert';
+const ListPage = () => {
+    const [filters, setFilters] = useState({});
+    const [page, setPage] = useState(1);
+    const { characters, loading, error, info } = useCharacters(filters, page);
+
+    const resetFilters = () => {
+        setFilters({});
+        setPage(1);
+    };
+    return (
+    <>
+    <div className="container my-4">
+        <h1 className="mb-3">Listado de Personajes</h1>
+        <FilterBar filters={filters} setFilters={setFilters} resetFilters={resetFilters} />
+        {loading && <LoadingSpinner />}
+        {error && <ErrorAlert message={error} />}
+
+        <div className="d-flex flex-wrap justify-content-start">
+            {characters.map(c => <CharacterCard key={c.id} character={c} />)}
+        </div>
+        <Pagination info={info} page={page} setPage={setPage} />
+    </div>
+    </>
+    );
+};
+
+export default ListPage;
