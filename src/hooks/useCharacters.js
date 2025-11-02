f4982d6eca2a13892a850eb88c8608c0b66850f7
Cris-div <christian.unocc@tecsup.edu.pe>
2025-11-02 00:49:31 -0500

    Hook useCharacters: estado, loading, error y paginación

--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -1,1 +1,28 @@
-//1
+import { useState,useEffect, use } from "react";
+import { getCharacters } from "../services/characterService";
+
+const useCharacters = (filters , page ) => {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [info, setInfo] = useState({});
+    useEffect(() => {
+        const fetcData = async () => {
+            setLoading(true);
+            setError(null);
+            try {
+                const data = await getCharacters({ ...filters, page });
+                setCharacters(data.results);
+                setInfo(data.info);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetcData();
+    }, [filters, page]);
+    return { characters, loading, error, info };
+};
+
+export default useCharacters;
