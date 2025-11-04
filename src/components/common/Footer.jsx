1a3ebb1445ce0c0cf74086c1b231074ca5a81851
yag-smith <yair.araujo@tecsup.edu.pe>
2025-11-04 15:28:21 -0500

    Agrega componente Footer con diseño simple y fondo oscuro para el pie de página

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,1 +1,10 @@
-//1
+import "../../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Rick & Morty App — Proyecto API React</p>
+      <p>Desarrollado por el equipo de Aplicaciones Empresariales Chiribayas 3</p>
+    </footer>
+  );
+}
