0803a71da9d4701bcef5b3523f99e55605093733
yag-smith <yair.araujo@tecsup.edu.pe>
2025-11-04 15:24:50 -0500

    Configura rutas principales con React Router e integra Navbar y Footer globales

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Importar páginas
+import HomePage from "./pages/HomePage";
+import ListPage from "./pages/ListPage";
 import ContactPage from "./pages/ContactPage";
-import Home from "./pages/HomePage";
+
+// Importar componentes comunes
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contacto" element={<ContactPage />} />
-      </Routes>
+      {/* NAVBAR - siempre visible */}
+      <Navbar />
+
+      {/* CONTENIDO CENTRAL */}
+      <main style={{ minHeight: "80vh" }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/lista" element={<ListPage />} />
+          <Route path="/contacto" element={<ContactPage />} />
+          <Route path="*" element={<h2 style={{ textAlign: "center", marginTop: "50px" }}>Página no encontrada</h2>} />
+        </Routes>
+      </main>
+
+      {/* FOOTER - siempre visible */}
+      <Footer />
     </BrowserRouter>
   );
 }
